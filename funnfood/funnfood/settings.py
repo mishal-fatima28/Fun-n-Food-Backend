566761071a6e6423637ebcff72055d3c5de7f042
mishal-fatima28 <mishalfatima28@gmail.com>
2022-05-20 22:52:57 +0500

    allowed hosts

--- funnfood/funnfood/settings.py
+++ funnfood/funnfood/settings.py
@@ -33,7 +33,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mishalfatima28@gmail.com'
 EMAIL_HOST_PASSWORD = 'przyskduzlncyifi'
 
-ALLOWED_HOSTS = ['127.0.0.1','fun-n-food.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','fun-n-food.herokuapp.com', 'www.funnfoodparks.com', 'funnfoodparks.com']
 
 
 # Application definition
